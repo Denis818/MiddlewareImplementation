68b214bdaa66c6d9b74251dca648197aad7e528a
Denis Pereira <denispereiradosreis@gmail.com>
2022-08-21 15:39:41 -0300

    Middleware Implementation

---
+++ Exceptions/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exceptions.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("Index")]
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+            ///return 
+        }
+
+        [HttpGet("Details")]
+        public ActionResult Details(int id)
+        {
+            throw new PlatformNotSupportedException();
+            //return View();
+        }
+
+        [HttpPost("Create")]
+        public ActionResult Create()
+        {
+            throw new NotFiniteNumberException();
+            //return View();
+        }
+
+    }
+}
---
+++ Exceptions/Extensions/MiddlewareCustomizado.cs
@@ -0,0 +1,25 @@
+﻿namespace Middleware.Example;
+
+public class MiddlewareCustomizado
+{
+    private readonly RequestDelegate _next;
+    protected readonly ILogger<Object> _logger;
+
+    public MiddlewareCustomizado(RequestDelegate next, ILogger<object> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError("Error: " + ex.Message);
+        }       
+    }
+}
